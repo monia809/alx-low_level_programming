5fc564fd25d5cd607698037038d4c43a591a71ff
monia809 <maa0192001@hotmail.com>
2023-09-18 13:23:16 -0700

    809

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _puts - prints a string, followed by a new line, to stdout
+ *
+ *  @str: to print
+ */
+
+void _puts(char *str);
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('/n');
+}
