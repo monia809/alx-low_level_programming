6501cf0606a6c61291b8aa0fc60eed0e29aa7cca
monia809 <maa0192001@hotmail.com>
2023-09-18 12:29:43 -0700

    809

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string
+ *
+ * Return: Lenngth of the string
+ */
+
+void reset_to_98(int *n)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}
