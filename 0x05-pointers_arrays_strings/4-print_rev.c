73c7debfbccf94b71a209c02bd9c804d24ba965b
monia809 <maa0192001@hotmail.com>
2023-09-18 15:51:45 -0700

    809

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,25 +2,24 @@
 
 /**
  * print_rev - prints a string in reverse, followed by a new line
- *
  * @s: string to be printed
  */
-
 void print_rev(char *s)
 {
-	int i = 0;
-	int o;
+	int i, j, len;
 
-	while (*s != '\0)
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		 i++;
-		 s++;
+		i++;
 	}
-	s--;
-	for (o = i; o > 0; o--)
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(*s);
-		s--;
+		_putchar(s[j]);
 	}
 
 	_putchar('\n');
