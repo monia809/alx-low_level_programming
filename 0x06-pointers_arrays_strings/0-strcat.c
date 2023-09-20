241345eb4fd044ec9407db1b1220b644e142e6f5
monia809 <maa0192001@hotmail.com>
2023-09-20 15:57:17 -0700

    809

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * _strcat - sequences two strings
+ *
+ * @dest: output dest
+ *
+ * @src: input source
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	for (x = 0; dest[x] != '/0'; x++)
+		;
+	for (y = 0; src[y] != '/0'; y++)
+	{
+		dest[x] = src[y];
+		x++;
+	}
+	return (dest);
+}
