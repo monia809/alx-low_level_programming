113d505aa7803d88f8ec769e686d3567415e9e25
monia809 <maa0192001@hotmail.com>
2023-09-20 17:24:45 -0700

    809

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ *
+ * @dest: input string
+ * @src: input string
+ * @n: input value
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; dest[x] != '\0' && src[x] != '\0'; x++)
+	{
+		if (dest[x] != src[x])
+			return (dest[x] - src[x]);
+	}
+	return (0);
+}
