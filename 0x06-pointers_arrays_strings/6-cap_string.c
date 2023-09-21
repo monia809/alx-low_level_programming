b06bb3a2b67d32b7a24b2e1e422bc01841981daf
monia809 <maa0192001@hotmail.com>
2023-09-21 13:36:43 -0700

    809

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
+ * cap_string - Capitalizes all words of a string
  *
- * @str: The string to be capitalized
- * @n: input string
+ * @str: The string to be capitalized.
  *
- * Return: caps on first letter of a separator
+ * Return: A pointer to the changed string.
  */
 
 char *cap_string(char *str)
