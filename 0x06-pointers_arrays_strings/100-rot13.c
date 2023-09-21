b0ab5ac4b380c6c7d4269e0c9a3b14b733d34515
monia809 <maa0192001@hotmail.com>
2023-09-21 14:08:16 -0700

    889

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *rot13 - encodes strings using rot13
+
+ *@s: pointer to string
+ *
+ *Return: pointer to encoded string
+ */
+
+char *rot13(char *str)
+{
+	int s, j;
+
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	s = 0;
+	while (strs] != '\0')
+	{
+		j = 0;
+		while (alpha[j] != '\0')
+		{
+			if (str[s] == alpha[j])
+			{
+				str[s] = rot[j];
+				break;
+			}
+			j++;
+		}
+		s++;
+	}
+	return (str);
+}
