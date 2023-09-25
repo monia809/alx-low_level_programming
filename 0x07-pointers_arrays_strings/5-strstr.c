93d76843807c462644e3f48572b0c95b860b2580
monia809 <maa0192001@hotmail.com>
2023-09-25 12:50:24 -0700

    809

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ *
+ *@s: first value
+ *@accept: second value
+ *
+ * Return: char with result
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int h = 0;
+
+	while (*s)
+	{
+		while (accept[h] != '\0')
+		{
+			if (*s == accept[h])
+				return (s);
+			h++;
+		}
+		h = 0;
+		s++;
+	}
+	return ('\0');
+}
