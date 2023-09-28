830cb0431f5c92994305728994656fe46423e8a9
monia809 <maa0192001@hotmail.com>
2023-09-28 06:20:01 -0700

    809

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *_evaluate - oh
+ *
+ *@i: int
+ *@n: int
+ *
+ *Return: 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - i
+ *
+ * @n: number
+ *
+ * @i: sad
+ *
+ * Return: 0
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (actual_sqrt_recursion(n, i + 1));
+}
