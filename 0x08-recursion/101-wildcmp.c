50d6e7bf85449448f92b326322b045e204d82942
monia809 <maa0192001@hotmail.com>
2023-09-28 15:32:46 -0700

    880

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compare
+ *
+ * @s1: p
+ * @s2: p
+ *
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}
