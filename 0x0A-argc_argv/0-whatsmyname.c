6a427023c2b80ec8ca56edc2be159411220316a9
monia809 <maa0192001@hotmail.com>
2023-10-02 12:47:27 -0700

    809

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - so
+ *
+ * @argc: numb of command
+ *
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
