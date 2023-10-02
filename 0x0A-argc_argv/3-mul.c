933983ad8848a610d5592bfe0d1ce5b4d10c2895
monia809 <maa0192001@hotmail.com>
2023-10-02 13:02:51 -0700

    809

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - monia
+ *
+ * @argc: is
+ *
+ * @argv: the
+ *
+ * Return: best
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
