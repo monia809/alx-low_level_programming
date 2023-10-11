3507c193b0aab45ffb2e6d9bf224d6abfbfdd6a2
monia809 <maa0192001@hotmail.com>
2023-10-11 11:54:37 -0700

    ui

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - c
+ *
+ * @argc: a
+ *
+ * @argv: a
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, nbytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
