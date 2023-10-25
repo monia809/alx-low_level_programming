866418d209cc0942bb721bfcf5c29346cb037c1d
monia809 <maa0192001@hotmail.com>
2023-10-24 17:09:51 -0700

    ma

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - function that frees a singly linked list
+ * pop_listint - function that deletes the head node of a listint_t,
+ * linked list.
+ * @head: pointer to pointer to the head of linked list.
  *
- * @head: pointer to pointer to the 
+ * Make sure there is no memory leaks.
  *
- * Return: non
+ * Return:  and returns the head node’s data (n) or 0 if list is empty.
  */
 
-void free_listint2(listint_t **head)
+int pop_listint(listint_t **head)
 {
-	listint_t *temp_variable;
+	listint_t *temp_variable; /* keep track of current head */
+	int data = 0;
 
-	if (head == NULL)
-		return; /* do anything */
-	while (*head != NULL)
-	{
-		temp_variable =  *head;
-		*head = (*head)->next;
-		free(temp_variable);
-	}
-	/* *head = NULL; */ /* not needed */
+    /* if there is no list */
+	if (head == NULL || *head == NULL)
+		return (0);
+	temp_variable = *head; /* stores the pointer to temp_variable head */
+    /* access the field n of temp_var and assign it to data */
+	data = temp_variable->n;
+    /* make the value at head be the value of head at next */
+	*head = (*head)->next;
+    /* free the temp var */
+	free(temp_variable);
+	return (data);
 }
